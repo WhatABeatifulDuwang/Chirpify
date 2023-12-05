eb54795bab9ee629b222bb0938357409a32336fd
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2023-12-05 14:05:31 +0100

    start home design

---
+++ Project/navbar.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="styles.css">
+<nav>
+    <ul>
+        <li>Chirpify.png</li>
+        <li><a href="home.php"></a>Home</li>
+        <li><a href="profile.php"></a>Profile</li>
+        <li>
+            More
+            <ul>
+                <li><a href="#">Lorem Ipsum</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+            </ul>
+        </li>
+        <li id="post"><button>Tweet button</button></li>
+    </ul>
+</nav>
