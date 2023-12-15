a507561952fe59b3e72c4063ea53bc8566283740
Quintenvddb <QBsmartguy@gmail.com>
2023-12-15 19:27:21 +0100

    Fixed Pathing

--- project/pages/profile-page.php
+++ project/pages/profile-page.php
@@ -3,8 +3,9 @@
     <head>
         <script src="../dependencies/scripts/profile-page_script.js"></script>
         <link rel="stylesheet" href="../dependencies/styles/profile-page_style.css">
+        <title>Proffile Page<titel>
     </head>
     <body>
-
+        <?php include "../dependencies/components/nav_bars/navbar.html" ?>
     </body>
 </html>
