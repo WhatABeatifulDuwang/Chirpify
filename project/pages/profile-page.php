29828e39c912aa1931eda02a07cdb265c1818d13
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2023-12-15 14:37:47 +0100

    Refactor pathing references

--- project/pages/profile-page.php
+++ project/pages/profile-page.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="dependencies/scripts/profile-page_script.js"></script>
-        <link rel="stylesheet" href="dependencies/styles/profile-page_style.css">
+        <script src="../dependencies/scripts/profile-page_script.js"></script>
+        <link rel="stylesheet" href="../dependencies/styles/profile-page_style.css">
     </head>
     <body>
 
