e292bf9c3b4d4703927898f37a211faec0e54f48
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2023-12-15 14:11:10 +0100

    Breaking update (Folder refractorization)

---
+++ project/pages/profile-page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="dependencies/scripts/profile-page_script.js"></script>
+        <link rel="stylesheet" href="dependencies/styles/profile-page_style.css">
+    </head>
+    <body>
+
+    </body>
+</html>
