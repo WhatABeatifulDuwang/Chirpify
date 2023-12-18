b61c33f9bd2ee546d3a5c655556f00216d29c445
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2023-12-18 08:52:07 +0100

    fixed hover on menu and pathing in js

--- project/dependencies/scripts/home_script.js
+++ project/dependencies/scripts/home_script.js
@@ -3,13 +3,13 @@
     let image = document.getElementById("heart");
 
      if (counter === 0) {
-         image.src = "assets/icons/heart-full-icon.png";
+         image.src = "../assets/icons/heart-full-icon.png";
          counter += 1;
 
          return counter;
      }
      else {
-         image.src = "assets/icons/heart-full-icon.png";
+         image.src = "../assets/icons/heart-full-icon.png";
          counter += 1;
 
          return counter;
