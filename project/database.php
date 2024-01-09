04d686655a689e3812cbf6205cc586bc3a37a1fe
Timo Korpershoek <timokorpershoek.info@gmail.com>
2024-01-09 11:03:06 +0100

    Re-started database.php

--- project/database.php
+++ project/database.php
@@ -1,8 +1,24 @@
 <?php
 
+// Database credentials
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "chirpify";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+// Creating the connection with the database
+try {
+    $conn = new PDO("mysql:host=$dbServername;dbname=$dbName", $dbUsername, $dbPassword);
+} catch (PDOException $e) {}
+
+function createUser($name, $last_name, $email, $password, $avatar = null, $admin = 0) {
+    global $conn;
+
+    $stmt = $conn->prepare("INSERT INTO users (name, last_name, email, password, avatar, admin) VALUES (?, ?, ?, ?, ?, ?)");
+    try {
+        $stmt->execute([$name, $last_name, $email, $password, $avatar, $admin]);
+        return $conn->lastInsertId();
+    } catch (PDOException $e) {
+        return false;
+    }
+}
