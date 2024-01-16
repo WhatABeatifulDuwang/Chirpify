4553149da88a240909d1a0ded52dd1ef9a5f3285
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2024-01-16 13:58:16 +0100

    login scherm UI update

--- project/pages/login-page.php
+++ project/pages/login-page.php
@@ -4,26 +4,28 @@
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <script src="dependencies/scripts/login-page_script.js"></script> 
+    <script src="dependencies/scripts/index_script.js"></script> 
     <link rel="stylesheet" href="../dependencies/styles/login-page_style.css"> 
-    <title></title> 
 </head> 
 <body> 
-    <div class="container"> 
+    <div> 
         <div class="login-box"> 
-            <div class="logo"> 
+           <div class="logo-wrapper">
+           <div class="logo"> 
                 <img src="../assets/imgs/logo_no_bg.png" alt="Chirpify Logo"> 
             </div> 
-            <h1>Login into Chirpify</h1> 
+           </div>
+            <h2>Login into Chirpify</h2> 
             <form id="loginForm" method="post"> 
-                <h3>Username or Email</h3> 
+                <div class="content-text">Username or Email</div>
                 <input type="text" id="username" name="username" placeholder="Enter your username or email"> 
                 <br> 
-                <h3>Password</h3> 
+                <div class="content-text">Password</div>
                 <input type="text" id="password" name="password" placeholder="Enter your password"> 
                 <br> 
                 <input type="submit" value="Login" class="btn btn-primary"> 
             </form> 
-            <p class="sign-up">Don't have an acount? <a href="placeholder-page.html">Sign Up</a></p> 
+            <p class="sign-up"> Don't have an acount? <a onclick="showPage('../dependencies\components\sidebars\login_side_bar.php')">Sign Up</a></p> 
             <div id="confirmationMessage"></div> 
         </div> 
     </div> 
