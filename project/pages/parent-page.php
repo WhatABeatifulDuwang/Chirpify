e1dc95a28714e1da3ce55d62d396967c06d9fda8
Quintenvddb <QBsmartguy@gmail.com>
2024-01-25 20:13:53 +0100

    Updated parent page and moved navbar into parent page

--- project/pages/parent-page.php
+++ project/pages/parent-page.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+if (!isset($_SESSION['user']['id']) || empty($_SESSION['user']['id'])) {
+    header('Location: login_page.php');
+    exit();
+}
+
+if (isset($_POST['logout'])) {
+    unset($_SESSION['user']['id']);
+    header('Location: login_page.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +23,39 @@
 <body>
 <div class="row">
     <div class="navbar">
-        <!--  This line of code assigns the navigation bar component to this div.  -->
-        <?php include "../dependencies/components/nav_bars/navbar.html" ?>
+    <nav>
+    <ul>
+        <li>
+            <img onclick="showPage('../pages/home.php')" class="icon" src="../assets/imgs/logo_no_bg.png" alt="bird-logo">
+        </li>
+            <li>
+                <button onclick="showPage('../pages/home.php')">
+                <img class="icon" src="../assets/imgs/house-icon-nobg.png" alt="house-icon"/>
+                Home
+                </button>
+            </li>
+        <li>
+            <button onclick="showPage('../pages/profile-page.php')">
+            <img class="icon" src="../assets/imgs/profile-icon.png" alt="profile-icon">
+            Profile
+            </button>
+        </li>
+        <li>
+            <div class="dropdown">
+                <img class="icon" src="../assets/imgs/elipsis-icon.png" alt="elipsis-icon">
+                <button>More</button>
+                <div class="dropdown-content">
+                    <a><button onclick="showPage('../pages/manage.php')">Manage</button></a>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                </div>
+            </div>
+        </li>
+        <form method="post">
+        <li><button type="submit" name="logout" id="post">logout</button></li>
+        </form>
+    </ul>
+</nav>
     </div>
     <div class="container">
         <iframe id="parent_div"></iframe>
